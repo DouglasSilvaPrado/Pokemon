036a26659913e7e96f0a099852bd5139bd6f5c4a
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-05-20 15:47:50 -0300

    Navbar end Footer

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,16 @@
 <template>
-  <div class="container">
-    <div class="row conteudo-tela-pokemons">
-      <!-- detalhes -->
-      <PokemonDetails :pokemonSelected="pokemonSelected" @on-add-pokemon="addPokemon" />
-
-      <!-- lista -->
-      <PokemonList :pokemons="pokemons" @on-select-pokemon="selectPokemon" />
-    </div>
-    <!-- time -->
-    <PokemonTeam :teamPokemon="myTeamPokemon" @on-remove-pokemon="removePokemon" />
-  </div>
+  <Navbar />
+  <router-view></router-view>
+  <Footer />
 </template>
 
-
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
-import IPokemon from "./interfaces/IPokemon";
-import PokemonList from "./components/PokemonList.vue";
-import api from "./services/api";
-import PokemonDetails from "./components/PokemonDetails.vue";
-import IPokemonDetails from "./interfaces/IPokemonDetails";
-import PokemonTeam from "./components/PokemonTeam.vue";
+import { defineComponent} from "vue";
+import Navbar from "./components/layout/Navbar.vue";
+import Footer from "./components/layout/Footer.vue";
 
 export default defineComponent({
-  name: "App",
-  setup() {
-    const pokemons = ref<IPokemon[]>([]);
-    const pokemonSelected = ref<IPokemonDetails>();
-    const myTeamPokemon = ref(<IPokemonDetails[]>[])
-
-    async function getPokemons(): Promise<void> {
-      api.getAll().then((res) => {
-        pokemons.value = res.data.results;
-      });
-    }
-
-    async function selectPokemon(pokemon: IPokemon) {
-      api
-        .getOne(pokemon.url)
-        .then((res) => {
-          pokemonSelected.value = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-
-    function addPokemon(pokemon: IPokemonDetails) {  
-      myTeamPokemon.value.push(pokemon);
-    }
-
-    function removePokemon(pokemon: IPokemonDetails) {
-      if(confirm(`Deseja remover o ${pokemon.name} do seu time ?`)){
-        myTeamPokemon.value = myTeamPokemon.value.filter((p) => p.name !== pokemon.name);
-      }
-      
-    }
-
-    onMounted(() => {
-      getPokemons();
-    });
-
-    return { pokemons, selectPokemon, pokemonSelected, addPokemon, myTeamPokemon, removePokemon };
-  },
-  components: { PokemonList, PokemonDetails, PokemonTeam }
+    name: "App",
+    components: { Navbar, Footer }
 });
 </script>
-
-<style scoped>
-.conteudo-tela-pokemons {
-  margin-bottom: 100px;
-}
-</style>
