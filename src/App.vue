a5eacfcc616405c54e772c0fe3bc5a9eaba76d09
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-05-20 13:24:03 -0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,59 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="container">
+    <div class="row conteudo-tela-pokemons">
+      <!-- detalhes -->
+      <PokemonDetails :pokemonSelected="pokemonSelected" />
+
+      <!-- lista -->
+      <PokemonList :pokemons="pokemons" @on-select-pokemon="selectPokemon" />
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import IPokemon from "./interfaces/IPokemon";
+import PokemonList from "./components/PokemonList.vue";
+import api from "./services/api";
+import PokemonDetails from "./components/PokemonDetails.vue";
+import IPokemonDetails from "./interfaces/IPokemonDetails";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const pokemons = ref<IPokemon[]>([]);
+    const pokemonSelected = ref<IPokemonDetails>();
+
+    async function getPokemons(): Promise<void> {
+      api.getAll().then((res) => {
+        pokemons.value = res.data.results;
+      });
+    }
+
+    async function selectPokemon(pokemon: IPokemon) {
+      api
+        .getOne(pokemon.url)
+        .then((res) => {
+          pokemonSelected.value = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    onMounted(() => {
+      getPokemons();
+    });
+
+    return { pokemons, selectPokemon, pokemonSelected };
+  },
+  components: { PokemonList, PokemonDetails,}
+});
+</script>
+
+<style scoped>
+.conteudo-tela-pokemons {
+  margin-bottom: 100px;
 }
 </style>
