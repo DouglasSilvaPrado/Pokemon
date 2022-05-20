9c63be4abc1c17a036d9522c4f03f5c5531e1617
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-05-20 13:21:52 -0300

    interfaces

---
+++ src/interfaces/IPokemon.ts
@@ -0,0 +1,5 @@
+export default interface IPokemon {
+    name: string;
+    url: string;
+    urlImgSvg: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world"
+}
---
+++ src/interfaces/IPokemonDetails.ts
@@ -0,0 +1,29 @@
+export default interface IPokemonDetails {
+    name: string;
+    id: number;
+    types: [
+        {
+            type: {
+                name: string
+            }
+        },
+        
+    ];
+    height: number;
+    weight: number;
+    abilities: [
+        {
+            id: number;
+            ability: {
+                name: string
+            }
+        }
+    ];
+    sprites: {
+        other:{
+            dream_world: {
+                front_default: string;
+            }
+        }
+    }
+}
