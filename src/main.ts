4d179a176c99ed039142b8179fa98d8ececea11f
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-05-24 11:30:24 -0300

    add store

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router'
+import { key, store } from './store'
 
 createApp(App)
     .use(router)
+    .use(store, key)
     .mount('#app')
