0ffb75b82ee1c644a72fe43c45df2a9683a1e552
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-05-24 11:32:06 -0300

    store

---
+++ src/store/index.ts
@@ -0,0 +1,62 @@
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import IPokemonDetails from "../interfaces/IPokemonDetails";
+import api from "../services/api";
+import { GET_TEAMS, SAVE_TEAM } from "./actions-type";
+import {
+  ADICIONA_POKEMON,
+  DEFINE_TEAMS,
+  REMOVER_POKEMON,
+} from "./mutations-type";
+
+interface Estado {
+  myTeamPokemon: IPokemonDetails[];
+  allTeams: IPokemonDetails[];
+}
+
+export const key: InjectionKey<Store<Estado>> = Symbol();
+
+export const store = createStore<Estado>({
+  state: {
+    myTeamPokemon: [],
+    allTeams: [],
+  },
+  mutations: {
+    [ADICIONA_POKEMON](state, pokemon: IPokemonDetails) {
+      const pokemonFormateed: IPokemonDetails = {
+        id: pokemon.id,
+        name: pokemon.name,
+        sprites: {
+          other: {
+            dream_world: {
+              front_default: pokemon.sprites.other.dream_world.front_default,
+            },
+          },
+        },
+      };
+      state.myTeamPokemon.push(pokemonFormateed);
+    },
+    [REMOVER_POKEMON](state, pokemon: IPokemonDetails) {
+      state.myTeamPokemon = state.myTeamPokemon.filter(
+        (p) => p.id !== pokemon.id
+      );
+    },
+    [DEFINE_TEAMS](state, teams: IPokemonDetails[]) {
+      state.allTeams = teams;
+    },
+  },
+  actions: {
+    [GET_TEAMS]({ commit }) {
+      api.getTeams().then((res) => {
+        commit(DEFINE_TEAMS, res.data);
+      });
+    },
+    [SAVE_TEAM](contexto, team: IPokemonDetails[]) {
+      api.saveTeam(team).then((res) => {});
+    },
+  },
+});
+
+export function useStore(): Store<Estado> {
+  return vuexUseStore(key);
+}
