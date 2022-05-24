a6ee61e0b87dcc39ae4b03f0d268d4fc296ccba6
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-05-24 11:32:35 -0300

    getTeams saveTeams api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,16 @@ import { http } from "./config";
 
 export default {
   getAll: () => {
-    return http.get(`pokemon?limit=32`);
+    return http.get(`pokemon?limit=152`);
   },
   getOne: (url: string) => {
     return http.get(`${url}`);
+  },
+  getTeams: () =>{
+    return http.get(`http://localhost:8080/teams`);
+  },
+  saveTeam: (team: any) =>{
+    return http.post(`http://localhost:8080/teams`, team);
   }
+
 };
